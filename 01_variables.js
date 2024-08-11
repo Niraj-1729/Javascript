d55f04cc6f31d469d2e4b484e5b948bee54b98e0
Niraj Kumar <nirajs9kr@gmail.com>
2024-08-11 21:52:04 +0530

    second commit

---
+++ 01_variables.js
@@ -0,0 +1,23 @@
+const account_Id=1445332
+let account_Email="nirajs9kr@gmail.com"
+var account_Password = "123456"
+let accountState;
+accountCity="Asansol"
+// account_Id=2 Not allowed to change the const variable
+
+account_Email="dotcom@gmail.com"
+account_Password="22324456"
+accountCity="Kolkata"
+
+console.table([account_Id,account_Email,accountState,account_Password])
+
+/* 
+1.const can't be changed 
+2.var and let can be changed 
+3.prefer not to use var, because of the issue in block scope and functional scope
+4.Use let **
+
+*/
+
+
+
