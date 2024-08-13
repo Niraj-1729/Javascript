fb814e71baa6e43946b22fb6cf3125352f4fef56
Niraj Kumar <nirajs9kr@gmail.com>
2024-08-13 19:51:46 +0530

    updates

---
+++ 02_data_Types.js
@@ -0,0 +1,29 @@
+// ECMA stadards 
+
+"use strict";   // treat all JS code as newer version 
+// alert(3 + 4) //we are using nodejs not browser 
+
+console.log(3 + 3);
+// code readibility should be high 
+console.log("Niraj The Great");
+
+let name = "Niraj"  // string datatype
+let age = 21       //  number datatype
+let isLoggedIn = false  // boolean datatype
+
+// nunmber=> 2^53
+// bigint
+// string => true/false
+// null=. standalone value
+// undefined =>
+// symbol =>unique
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null);
+
+
+
+
+
---
+++ 04_comparisons.js
@@ -0,0 +1,24 @@
+//******** Comparing same data type ***************//
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+//******** Comparing same data type ***************//
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//**(()**) */ == checks value and === checks value and datatype 
+console.log("2"===2)
