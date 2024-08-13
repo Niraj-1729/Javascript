f0385735491466931f7cb16fac40e4382ec14369
Niraj Kumar <nirajs9kr@gmail.com>
2024-08-13 22:25:58 +0530

    updates

---
+++ 05_datatpyes-summary.js
@@ -0,0 +1,36 @@
+// primitive
+
+// 7 types:- String, number, boolean, null, undefined, symbol
+
+// Reference type:- Non-Premitive 
+
+// Arrays, Objects, Functions
+
+const score = 100
+const scoreValue = 0.3
+const isALoggedIn = false
+const outsideTemp = null
+let usermail=undefined
+const id=symbol('123')
+const anotherId=symbol('123')
+
+console.log(id===anotherId)
+const bigNumber=1234653434434
+
+//Array
+// Array=[]
+const heros=["Sunil", "Suraj", "Niraj"]
+
+//Object
+let myObj={
+    name:"Niraj",
+    roll:48,
+}
+
+// Function
+const myFunction=function(){
+    console.log("Hello world")
+}
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+ 
