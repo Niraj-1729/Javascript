ecffee12dfdb79d6a639ac73f9dc0b3b5351beb1
Niraj Kumar <nirajs9kr@gmail.com>
2024-08-15 18:48:42 +0530

    updates

---
+++ 02_Array/01_Arrays.js
@@ -0,0 +1,31 @@
+const myArr=[5,1,2,3,4,5]
+const myHeros=["Shaktiman", "naagraj"]
+
+// console.log(myArr[0]);
+
+// Declaration of Array 
+const  myArr2=new Array(1,2,3,4)
+
+// Arr methods 
+// myArr.push(6)
+// console.log(myArr);
+myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(9)
+// console.log(myArr);
+
+// slice and splice 
+console.log("A", myArr)
+const myn1=myArr.slice(1,3)
+console.log(myn1)
+console.log("B", myArr);
+
+const myn2=myArr.splice(1,3)
+console.log("C", myArr)
+
+
+
+
+
+
